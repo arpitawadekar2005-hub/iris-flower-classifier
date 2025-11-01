60328c103e6b46b3993a81244c355c3d680eb911
arpitawadekar2005-hub <arpitawadekar2005@gmail.com>
2025-11-01 22:22:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ st.title("ML Prediction App")
 
 # Example: suppose your model takes 4 input features
 st.header("Enter Input Features")
-f1 = st.number_input("Feature 1", value=0.0)
-f2 = st.number_input("Feature 2", value=0.0)
-f3 = st.number_input("Feature 3", value=0.0)
-f4 = st.number_input("Feature 4", value=0.0)
+f1 = st.number_input("Sepal Lenght in cms", value=0.0)
+f2 = st.number_input("Sepal Width in cms", value=0.0)
+f3 = st.number_input("Petal Lenght in cms", value=0.0)
+f4 = st.number_input("Pepal Width in cms", value=0.0)
 
 # Predict button
 if st.button("Predict"):
